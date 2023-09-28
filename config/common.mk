8a3bbd3b01409cc20b968ef89115d3790aa809fd
SirRGB <sirrgb@proton.me>
2023-09-28 16:12:57 +0200

    prebuilt: add Aperture by default

--- config/common.mk
+++ config/common.mk
@@ -316,6 +316,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
 
+# QTI Perf
 ifeq ($(BOARD_USES_QCOM_HARDWARE),)
 -include vendor/qcom/common/perf/perf-vendor.mk
 TARGET_COMMON_QTI_COMPONENTS := perf
@@ -323,4 +324,10 @@ TARGET_COMMON_QTI_COMPONENTS := perf
 PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
+endif
+
+# Camera
+ifneq ($(TARGET_APERTURE_OPTOUT),true)
+PRODUCT_PACKAGES += \
+    Aperture
 endif
