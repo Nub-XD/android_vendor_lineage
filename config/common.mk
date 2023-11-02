1cf9a45334c615647f44a3b72483e44d0b39f0e1
ImSpiDy <SpiDy2713@gmail.com>
2023-11-02 06:53:08 +0000

    Revert "config: Include common qti-perf blobs"
    This reverts commit 659246bb7e68fa1aac009139d392447a27f682b6.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -315,12 +315,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),)
--include vendor/qcom/common/perf/perf-vendor.mk
-TARGET_COMMON_QTI_COMPONENTS := perf
-
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
-endif
