350ec8565ebf6ad8639783879e15887bf9402f15
ImSpiDy <SpiDy2713@gmail.com>
2023-11-02 06:53:05 +0000

    Revert "prebuilt: add Aperture by default"
    This reverts commit 8a3bbd3b01409cc20b968ef89115d3790aa809fd.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -316,7 +316,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
 
-# QTI Perf
 ifeq ($(BOARD_USES_QCOM_HARDWARE),)
 -include vendor/qcom/common/perf/perf-vendor.mk
 TARGET_COMMON_QTI_COMPONENTS := perf
@@ -324,10 +323,4 @@ TARGET_COMMON_QTI_COMPONENTS := perf
 PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
-endif
-
-# Camera
-ifneq ($(TARGET_APERTURE_OPTOUT),true)
-PRODUCT_PACKAGES += \
-    Aperture
 endif
